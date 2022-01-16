7cdc485728953ca854e902ebc79d9cbc37c3b33c
gajalakc <gajalakshan.chandrasegaran@metropolia.fi>
2022-01-16 18:09:30 +0200

    add new file call variables and update list file

--- components/List.js
+++ components/List.js
@@ -1,33 +1,41 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
+import {baseUrl} from '../utils/variables';
 import ListItem from './ListItem';
-// remove the local array list and add new 
-const dataUrl =
-  'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
 
 const List = () => {
   const [mediaArray, setMediaArray] = useState([]);
-  const loadMedia = async () => {
+  const loadMedia = async (start = 0, limit = 10) => {
     try {
-      const response = await fetch(dataUrl);
+      const response = await fetch(
+        `${baseUrl}media?start=${start}&limit=${limit}`
+      );
       if (!response.ok) {
         throw Error(response.statusText);
       }
       const json = await response.json();
-      setMediaArray(json);
+      const media = await Promise.all(
+        json.map(async (item) => {
+          const response = await fetch(baseUrl + 'media/' + item.file_id);
+          const mediaData = await response.json();
+          return mediaData;
+        })
+      );
+      setMediaArray(media);
       console.log(mediaArray);
     } catch (error) {
       console.error(error);
     }
   };
+ 
   useEffect(() => {
-    loadMedia();
+    loadMedia(0, 5);
   }, []);
 
   return (
     <FlatList
       data={mediaArray}
-      keyExtractor={(item) => item.title}
+      keyExtractor={(item) => item.file_id.toString()}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     ></FlatList>
   );
