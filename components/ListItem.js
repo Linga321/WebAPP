bf019ba9edad68b92c2f9547fbb7cac117b9dadd
Linga321 <56063292+Linga321@users.noreply.github.com>
2022-01-16 18:18:57 +0200

    Update ListItem.js
    Update and fix issue

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {uploadsUrl} from '../utils/variables';
 import PropTypes from 'prop-types';
 
 const ListItem = (props) => {
